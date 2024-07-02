465352b817331ca64202f74b2dd420762b8b680d
saniaissac <saniaissac@gmail.com.com>
2024-07-02 15:56:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card1 from './Components/Card1'
+import AppBar2 from './Components/AppBar2'
+
+const App = () => {
+  return (
+    <div>
+      < AppBar2/>
+      <br/>
+      <br/>
+      <Card1/>
+    </div>
+  )
+}
+
+export default App
